6b711c6c16605a014910ac4c9ee4e05d1c1b6808
andytheelf <ballew22@gmail.com>
2020-04-05 12:40:34 -0500

    Variable set up

---
+++ Develop/script.js
@@ -0,0 +1,27 @@
+//Password Variables
+var inputCharacters = "";
+var prompts = "";
+
+//Variable Character Sets
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var specialSet = "@#$%^&*+<>?[]{}=~/";
+  var numericValue = "0123456789";
+
+ 
+
+
+// Get references to the #generate element
+ var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
